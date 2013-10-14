0382933b312f94c65f7c8cb66dc5bc3ca2a28e94
anhnt <tuananh00473@gmail.com>
2013-10-14 09:57:54 +0700

    commit code

--- src/com/qsoft/ondio/activity/LoginActivity.java
+++ src/com/qsoft/ondio/activity/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.qsoft.ondio.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import com.qsoft.ondio.R;
 
 /**
@@ -11,9 +14,21 @@ import com.qsoft.ondio.R;
  */
 public class LoginActivity extends Activity
 {
+    private Button btLogin;
+
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        btLogin = (Button) findViewById(R.id.login_button_next);
+        btLogin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+            }
+        });
+
     }
 }
