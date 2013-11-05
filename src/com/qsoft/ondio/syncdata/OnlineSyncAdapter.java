896148c6765a668312694021dd4ff85ac2b26d5f
anhnt <tuananh00473@gmail.com>
2013-11-05 15:59:29 +0700

    commit code

--- src/com/qsoft/ondio/syncdata/OnlineSyncAdapter.java
+++ src/com/qsoft/ondio/syncdata/OnlineSyncAdapter.java
@@ -39,8 +39,7 @@ public class OnlineSyncAdapter extends AbstractThreadedSyncAdapter
             {
                 authToken = cToken.getString(cToken.getColumnIndex(Constants.USER_ACCESS_TOKEN));
             }
-//            String authToken = mAccountManager.blockingGetAuthToken(account, Constants.AUTHTOKEN_TYPE_FULL_ACCESS, true);
-//            String userObjectId = mAccountManager.getUserData(account, Constants.USERDATA_USER_OBJ_ID);
+            String userObjectId = mAccountManager.getUserData(account, Constants.USERDATA_USER_OBJ_ID);
 
             Log.d(TAG, " ==> Get data from service.");
 
@@ -48,96 +47,67 @@ public class OnlineSyncAdapter extends AbstractThreadedSyncAdapter
             ArrayList<Feed> remoteFeeds = Constants.sServerAuthenticate.getHomeFeed(authToken);
             Log.d(TAG, "data : " + remoteFeeds);
 
-
-            provider.delete(Constants.CONTENT_URI_FEED, null, null);
-            Log.d(TAG, "delete");
-            for (Feed feed : remoteFeeds)
+            Log.d(TAG, " ==> Get data from local.");
+            // Get shows from local
+            ArrayList<Feed> localFeeds = new ArrayList<Feed>();
+            Cursor c = provider.query(Constants.CONTENT_URI_FEED, null, null, null, "_ID");
+            if (c != null)
             {
-                provider.insert(Constants.CONTENT_URI_FEED, feed.getContentValues());
+                while (c.moveToNext())
+                {
+                    localFeeds.add(Feed.fromCursor(c));
+                }
+                c.close();
             }
 
-//            Log.d(TAG, " ==> Get data from local.");
-//            // Get shows from local
-//            ArrayList<Feed> localFeeds = new ArrayList<Feed>();
-//            Cursor c = provider.query(Constants.CONTENT_URI_FEED, null, null, null, "_ID");
-//            if (c != null)
-//            {
-//                while (c.moveToNext())
-//                {
-//                    localFeeds.add(Feed.fromCursor(c));
-//                }
-//                c.close();
-//            }
-//
-//            for (Feed feed : remoteFeeds)
-//            {
-//                if (!localFeeds.contains(feed))
-//                {
-//                    provider.insert(Constants.CONTENT_URI_FEED, feed.getContentValues());
-//                }
-////                provider.delete(Constants.CONTENT_URI_FEED, null, null);
-////                Log.d(TAG, "delete");
-////                provider.insert(Constants.CONTENT_URI_FEED, feed.getContentValues());
-//            }
-//
-//            for (Feed feed : localFeeds)
-//            {
-//                if (!remoteFeeds.contains(feed))
-//                {
-//                    provider.delete(Constants.CONTENT_URI_FEED, "id", String[]{"",""} );
-//                }
-//            }
+            // See what Local shows are missing on Remote
+            ArrayList<Feed> showsToRemote = new ArrayList<Feed>();
+            for (Feed localFeed : localFeeds)
+            {
+                if (!remoteFeeds.contains(localFeed))
+                {
+                    showsToRemote.add(localFeed);
+                }
+            }
 
-//
+            // See what Remote shows are missing on Local
+            ArrayList<Feed> showsToLocal = new ArrayList<Feed>();
+            for (Feed remoteFeed : remoteFeeds)
+            {
+                if (!localFeeds.contains(remoteFeed)) // TODO REMOVE THIS
+                {
+                    showsToLocal.add(remoteFeed);
+                }
+            }
 
-//            // See what Local shows are missing on Remote
-//            ArrayList<Feed> showsToRemote = new ArrayList<Feed>();
-//            for (Feed localFeed : localFeeds)
-//            {
-//                if (!remoteFeeds.contains(localFeed))
-//                {
-//                    showsToRemote.add(localFeed);
-//                }
-//            }
+            if (showsToRemote.size() == 0)
+            {
+                Log.d("udinic", TAG + "> No local changes to update server");
+            }
+            else
+            {
+                Log.d("udinic", TAG + "> Updating remote server with local changes");
 
-//            // See what Remote shows are missing on Local
-//            ArrayList<Feed> showsToLocal = new ArrayList<Feed>();
-//            for (Feed remoteFeed : remoteFeeds)
-//            {
-//                if (!localFeeds.contains(remoteFeed)) // TODO REMOVE THIS
-//                {
-//                    showsToLocal.add(remoteFeed);
-//                }
-//            }
+                // Updating remote tv shows
+                for (Feed remoteFeed : showsToRemote)
+                {
+                    Log.d("udinic", TAG + "> Local -> Remote [" + remoteFeed.display_name + "]");
+                    Constants.sServerAuthenticate.putShow(authToken, userObjectId, remoteFeed);
+                }
+            }
 
-//            if (showsToRemote.size() == 0)
-//            {
-//                Log.d("udinic", TAG + "> No local changes to update server");
-//            }
-//            else
-//            {
-//                Log.d("udinic", TAG + "> Updating remote server with local changes");
-//
-//                // Updating remote tv shows
-//                for (Feed remoteFeed : showsToRemote)
-//                {
-//                    Log.d("udinic", TAG + "> Local -> Remote [" + remoteFeed.display_name + "]");
-//                    Constants.sServerAuthenticate.putShow(authToken, userObjectId, remoteFeed);
-//                }
-//            }
-
-//            if (showsToLocal.size() == 0)
-//            {
-//                Log.d(TAG, " ==> No server changes to update local database");
-//            }
-//            else
-//            {
-//                Log.d(TAG, " ==> Updating local database with remote changes");
-//                for (Feed localFeed : showsToLocal)
-//                {
-//                    provider.insert(Constants.CONTENT_URI_FEED, localFeed.getContentValues());
-//                }
-//            }
+            if (showsToLocal.size() == 0)
+            {
+                Log.d(TAG, " ==> No server changes to update local database");
+            }
+            else
+            {
+                Log.d(TAG, " ==> Updating local database with remote changes");
+                for (Feed localFeed : showsToLocal)
+                {
+                    provider.insert(Constants.CONTENT_URI_FEED, localFeed.getContentValues());
+                }
+            }
 
             Log.d(TAG, " ==> Finished.");
 
